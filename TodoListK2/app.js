eba40b0559e23adbf1e3597a7462746b5aa35ed3
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2021-01-14 10:03:00 +0000

    Todo List 2K4

--- TodoListK2/app.js
+++ TodoListK2/app.js
@@ -10,7 +10,17 @@ while (input !== 'quit' && input !== "q") {
         console.log("********************************");
     } else if (input === "new") {
         const newTodo = prompt("OK, what is the new todo?");
-        todos.push(newTodo);        
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list.`);
+    } else if (input === "delete") {
+        const index = parseInt(prompt("OK, enter an index to delete: "));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`OK, deleted ${deleted[0]}`);            
+        } else {
+            console.log("Unknow index.. .");
+        }
+        
     }
     input = prompt("What would you like to do?");
 }
